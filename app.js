8148df9f6b475e5755296dac998f3101f21cc72d
RGPin <157803400+RGPin@users.noreply.github.com>
2026-07-31 14:15:10 +0800

    prepare for deploy

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.set("trust proxy", 1);
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
